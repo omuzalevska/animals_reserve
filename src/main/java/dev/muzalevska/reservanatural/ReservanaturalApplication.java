f57c72eb87e26662fbe75eb5e6bebf6f0a239c76
OMuzalevska <oksana.muzalevskaja@gmail.com>
2025-01-17 14:51:29 +0100

    first comit

---
+++ src/main/java/dev/muzalevska/reservanatural/ReservanaturalApplication.java
@@ -0,0 +1,13 @@
+package dev.muzalevska.reservanatural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservanaturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservanaturalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/muzalevska/reservanatural/ReservanaturalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.muzalevska.reservanatural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservanaturalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
