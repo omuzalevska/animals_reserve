84a42ff4c90b95a5230ec4c2e387f895140cf592
OMuzalevska <oksana.muzalevskaja@gmail.com>
2025-01-29 00:23:16 +0100

    add count() & find by name animal

--- src/main/java/dev/muzalevska/reservanatural/controllers/AnimalController.java
+++ src/main/java/dev/muzalevska/reservanatural/controllers/AnimalController.java
@@ -1,6 +1,8 @@
 package dev.muzalevska.reservanatural.controllers;
 
 import dev.muzalevska.reservanatural.dto.AnimalDTO;
+import dev.muzalevska.reservanatural.entity.Animal;
+import dev.muzalevska.reservanatural.repository.AnimalRepository;
 //import dev.muzalevska.reservanatural.entity.Animal;
 import dev.muzalevska.reservanatural.services.AnimalService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,41 +17,58 @@ import java.util.List;
 public class AnimalController {
 
     private final AnimalService animalService;
+    
+    @Autowired
+    private AnimalRepository animalRepository;
 
     @Autowired
     public AnimalController(AnimalService animalService) {
         this.animalService = animalService;
     }
 
-    // Отримати всі тварини
+
+
+    // список всіх тварини
     @GetMapping
     public ResponseEntity<List<AnimalDTO>> getAllAnimals() {
         List<AnimalDTO> animals = animalService.getAllAnimals();
         return ResponseEntity.ok(animals);
     }
 
-    // Додати нову тварину
+    //к-ть тварин
+    @GetMapping("/count")
+    public Long getAnimalCount() {
+        return animalRepository.count();
+    }
+
+    // пошук по імені
+    @GetMapping("/name/{name}")
+    public List<Animal> getAnimalByName(@PathVariable String name) {
+        return animalRepository.findByName(name);
+    }
+
+    // Додати
     @PostMapping
     public ResponseEntity<AnimalDTO> createAnimal(@RequestBody AnimalDTO animalDTO) {
         AnimalDTO savedAnimal = animalService.save(animalDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedAnimal);
     }
 
-    // Отримати тварину за ID
+    // Отримати за ID
     @GetMapping("/{id}")
     public ResponseEntity<AnimalDTO> getAnimalById(@PathVariable Long id) {
         AnimalDTO animalDTO = animalService.getAnimalById(id);
         return ResponseEntity.ok(animalDTO);
     }
 
-    // Оновити тварину
+    // Оновити
     @PutMapping("/{id}")
     public ResponseEntity<AnimalDTO> updateAnimal(@PathVariable Long id, @RequestBody AnimalDTO updatedAnimalDTO) {
         AnimalDTO updatedAnimal = animalService.update(id, updatedAnimalDTO);
         return ResponseEntity.ok(updatedAnimal);
     }
 
-    // Видалити тварину
+    // Видалити
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteAnimal(@PathVariable Long id) {
         animalService.delete(id);
--- src/main/java/dev/muzalevska/reservanatural/repository/AnimalRepository.java
+++ src/main/java/dev/muzalevska/reservanatural/repository/AnimalRepository.java
@@ -1,10 +1,13 @@
 package dev.muzalevska.reservanatural.repository;
 
 import dev.muzalevska.reservanatural.entity.Animal;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
-    // методи для фільтрації
+    List<Animal> findByName(String name);
 }
